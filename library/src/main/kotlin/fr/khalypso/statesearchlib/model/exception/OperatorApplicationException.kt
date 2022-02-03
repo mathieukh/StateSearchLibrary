b18caec839753404e1afcd877719b3717a32caa9
Mathieu KHALEM <mathieu.khalem@gmail.com>
2022-02-03 10:36:50 +0100

    Add a sub-module preparing the examples one

--- library/src/main/kotlin/fr/khalypso/statesearchlib/model/exception/OperatorApplicationException.kt
+++ library/src/main/kotlin/fr/khalypso/statesearchlib/model/exception/OperatorApplicationException.kt
@@ -3,6 +3,6 @@ package fr.khalypso.statesearchlib.model.exception
 import java.lang.RuntimeException
 
 /**
- * Exception thrown when an [Operator] can not be applied to a [State].
+ * Exception thrown when an [fr.khalypso.statesearchlib.model.Operator] can not be applied to a [fr.khalypso.statesearchlib.model.State].
  */
 class OperatorApplicationException : RuntimeException("The operator can not be applied to the current state")
